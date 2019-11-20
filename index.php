66c94e8319de943b678dd25cb2dc04b84e934b21
brownadam <brownadam@dublinusd.org>
2019-11-20 19:09:34 +0000

    one

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Insert Name & Period Here]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Brown_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="#">Projects.html</a></li></br>
+      <li><a href="#">Resume.html</a></li></br>
+      <li><a href="#">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
